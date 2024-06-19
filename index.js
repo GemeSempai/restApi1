403d79d46d082efb0273e91ff3b3d032c89cefd8
GemeSempai <mike_veco@hotmail.com>
2024-06-19 14:28:17 -0600

    Added database config

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config({path: '.env'})
+const { checkDB } = require('./src/database.config');
 
+checkDB()
+
+console.log(process.env.APP_PORT)
+const PORT = process.env.APP_PORT | 4000
 const app = express()
 
 app.get("/",(req,res) => {
     res.send("hola")
 })
 
-app.listen(3000,()=>{
-    console.log("Hola putas")
+app.listen(PORT,()=>{
+    console.log(`Servidor en el puerto ${PORT}`)
 })
